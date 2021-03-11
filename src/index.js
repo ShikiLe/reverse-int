0c371d77e1bc32d2c4e988f7432c54736010e399
Elena Karpovich <mariajane1897@gmail.com>
2021-03-11 14:06:06 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    
+        let array = n.toString().split('');
+        let reverseArr =  []; 
+        for (let i = array.length - 1; i >= 0; i--) {
+            reverseArr.push(array[i]);
+        }
+        let stringInt = reverseArr.join('');
+        return Number.parseInt(stringInt);
+    
 }
